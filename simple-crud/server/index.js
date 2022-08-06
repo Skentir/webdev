451ad01ccfff39f610d84e3168eb53141380b50f
Kirsten S <daniellesison19@gmail.com>
2022-08-07 03:43:54 +0800

    initial commit

---
+++ simple-crud/server/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'admin123',
+    database: 'employeeSystem'
+});
+
+app.post('/create', (req,res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+
+    db.query(
+        "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+        [name,age,country,position,wage], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send("Values Inserted");
+            }
+        }
+    );
+});
+
+app.get('/',(req,res)=> {
+    console.log("Main");
+})
+
+app.listen(3001, ()=> {
+    console.log("Server is running on port 3001");
+});
