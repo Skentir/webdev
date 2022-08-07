19936e76e5059687bdbdff586a17078dda9edd1f
Kirsten S <daniellesison19@gmail.com>
2022-08-08 02:26:20 +0800

    Added update and delete

--- simple-crud/server/index.js
+++ simple-crud/server/index.js
@@ -26,15 +26,52 @@ app.post('/create', (req,res) => {
             if (err) {
                 console.log(err);
             } else {
-                res.send("Values Inserted");
+                res.send(result);
             }
         }
     );
 });
 
-app.get('/',(req,res)=> {
-    console.log("Main");
-})
+app.get('/employees',(req,res)=> {
+    db.query("SELECT * FROM employees", (err,result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.put('/update', (req,res) => {
+    const id = req.body.id;
+    const wage = req.body.wage;
+    db.query(
+        "UPDATE employees SET wage = ? WHERE id = ?",
+        [wage, id], 
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        }
+    );
+});
+
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id
+    db.query(
+        "DELETE FROM employees WHERE id = ?",
+        id,
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        }
+    )
+});
 
 app.listen(3001, ()=> {
     console.log("Server is running on port 3001");
