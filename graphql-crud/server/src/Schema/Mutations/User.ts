483aee42f201c6cad5cbe21048a7dfba369f800f
Kirsten S <daniellesison19@gmail.com>
2022-08-10 03:24:33 +0800

    ✨: add update password and delete user functionality

--- graphql-crud/server/src/Schema/Mutations/User.ts
+++ graphql-crud/server/src/Schema/Mutations/User.ts
@@ -1,5 +1,6 @@
-import { GraphQLString} from 'graphql';
+import { GraphQLID, GraphQLString} from 'graphql';
 import { UserType } from "../TypeDefs/User";
+import { MessageType } from "../TypeDefs/Messages";
 import {Users} from '../../Entities/Users';
 
 export const CREATE_USER =  {
@@ -14,5 +15,45 @@ export const CREATE_USER =  {
         const { name, username, password } = args;
         await Users.insert({ name, username, password });
         return args
+    }
+}
+
+export const UPDATE_PASSWORD = {
+    type: MessageType,
+    args: {
+        username: { type: GraphQLString },
+        oldPassword: { type: GraphQLString },
+        newPassword: { type: GraphQLString }
+    },
+    
+    async resolve(parent: any, args: any) {
+        const { username, oldPassword, newPassword } = args
+        const user = await Users.findOneBy({username: username})
+
+        if (!user) {
+            throw new Error("Username doesn't exist")
+        }
+        const userPassword = user?.password
+
+        if (oldPassword === userPassword) {
+            await Users.update({username: username}, {password: newPassword})
+            return { successful: true, message: "Password has been updated" }
+        } else {
+            throw new Error("Passwords do not match!")
+        }
+    }
+}
+
+export const DELETE_USER = {
+    type: MessageType,
+    args: {
+        id: { type: GraphQLID }
+    },
+    
+    async resolve(parent: any, args: any) {
+        const id = args.id;
+        await Users.delete(id);
+
+        return {successful: true, message: "Delete successful"}
     }
 }
--- graphql-crud/server/src/Schema/index.ts
+++ graphql-crud/server/src/Schema/index.ts
@@ -1,6 +1,6 @@
 import { GraphQLObjectType, GraphQLSchema } from "graphql";
 import { GET_ALL_USERS } from './Queries/User';
-import { CREATE_USER } from './Mutations/User';
+import { CREATE_USER, DELETE_USER, UPDATE_PASSWORD } from './Mutations/User';
 
 const RootQuery = new GraphQLObjectType({
     name: "RootQuery",
@@ -13,7 +13,9 @@ const Mutation = new GraphQLObjectType({
     name: "Mutation",
     fields: {
         createUser: CREATE_USER,
-    }
+        deleteUser: DELETE_USER,
+        updatePassword: UPDATE_PASSWORD
+    },
 })
 
 export const schema = new GraphQLSchema({
