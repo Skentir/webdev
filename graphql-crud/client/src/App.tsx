1840fe13f8a8fef417adf734ddce4203a86c3367
Kirsten S <daniellesison19@gmail.com>
2022-08-10 03:44:31 +0800

    ✨: setup react client

---
+++ graphql-crud/client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
